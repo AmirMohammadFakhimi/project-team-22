e6a1769e171a3a770a5a0cc27a66e9d6d8a81f41
parsasharify <sharifyparsa1381@gmail.com>
2021-04-16 00:03:24 -0700

    mymenu

---
+++ src/main/java/controller/ProfileController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import java.util.regex.Matcher;
+
+public class ProfileController {
+    protected String input;
+    protected String[] splited;
+
+    public ProfileController(String input) {
+        this.input = input;
+    }
+
+    public Enum changeNickName(String input) {
+    }
+
+    public Enum checkchangeNickName(Matcher matcher, Player player) {
+    }
+
+    public Enum changePassWord(String input) {
+    }
+
+    public Enum checkchangePassWord(Matcher matcher, Player player) {
+    }
+}
