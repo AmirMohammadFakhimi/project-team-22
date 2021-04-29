e21cdd9bd13c5e2f39cc784b34c0b4cce2cf634c
Amir Mohammad Fakhimi <79265203+AmirMohammadFakhimi@users.noreply.github.com>
2021-04-30 00:13:44 +0430

    Add files via upload

---
+++ src/main/java/controller/DuelMenuController.java
@@ -0,0 +1,140 @@
+package controller;
+
+import model.Player;
+
+public class DuelMenuController {
+//    TODO: how to handle board
+//    TODO: how to handle graveyard --> ArrayList --> put it in Player class/DuelMenuController/board class??
+    private Player turnPlayer;
+    private Player notTurnPlayer;
+    private Enum phase;
+
+    public DuelMenuController(Player turnPlayer, Player notTurnPlayer, Enum phase) {
+        setTurnPlayer(turnPlayer);
+        setNotTurnPlayer(notTurnPlayer);
+        setPhase(phase);
+    }
+
+    public void setTurnPlayer(Player turnPlayer) {
+        this.turnPlayer = turnPlayer;
+    }
+
+    public void setNotTurnPlayer(Player notTurnPlayer) {
+        this.notTurnPlayer = notTurnPlayer;
+    }
+
+    public void setPhase(Enum phase) {
+        this.phase = phase;
+    }
+
+    public Enum findCommand(String command) {
+
+    }
+
+    private Enum selectCard(String command) {
+
+    }
+
+    private Enum checkSelectCard(String command) {
+
+    }
+
+    private Enum deselectCard(String command) {
+
+    }
+
+    private Enum checkDeselectCard(String command) {
+
+    }
+
+    private Enum summonMonster(String command) {
+//        TODO: maybe change the name to --> summonCard
+    }
+
+    private Enum checkSummonMonster(String command) {
+//        TODO: maybe change the name to --> checkSummonCard
+    }
+
+    private void victimize() {
+//        TODO: handle it!
+    }
+
+    private Enum setAMonster(String command) {
+
+    }
+
+    private Enum checkSetAMonster(String command) {
+
+    }
+
+    private Enum changePosition(String command) {
+
+    }
+
+    private Enum checkChangePosition(String command) {
+
+    }
+
+    private void updateGraveyard() {
+//        TODO: handle it!
+    }
+
+    private Enum flipSummon(String command) {
+
+    }
+
+    private Enum checkFlipSummon(String command) {
+
+    }
+
+    private Enum attack(String command) {
+
+    }
+
+    private Enum checkAttack(String command) {
+//        TODO: maybe merge with checkDirectAttack function
+    }
+
+    private Enum directAttack(String command) {
+
+    }
+
+    private Enum checkDirectAttack(String command) {
+//        TODO: maybe merge with checkAttack function
+    }
+
+    private Enum activeASpellCard(String command) {
+
+    }
+
+    private Enum checkActiveASpellCard(String command) {
+
+    }
+
+    private Enum setASpell(String command) {
+
+    }
+
+    private Enum checkSetASpell(String command) {
+
+    }
+
+//    TODO: handle activeASpellInOpponentTurn
+//    TODO: -----------------------------------------------
+//    TODO: we are not sure about under functions
+
+    private Enum ritualSummon(String command) {
+
+    }
+
+    private Enum specialSummon(String command) {
+
+    }
+
+    private Player/*or Enum*/ checkWinner() {
+
+    }
+
+//    TODO: -----------------------------------------------
+
+}
---
+++ src/main/java/controller/ShopMenuController.java
@@ -0,0 +1,49 @@
+package controller;
+
+import model.Card;
+import model.Player;
+
+import java.util.regex.Matcher;
+
+public class ShopMenuController {
+    private Player loggedInPlayer;
+
+    public ShopMenuController(Player loggedInPlayer) {
+        setLoggedInPlayer(loggedInPlayer);
+    }
+
+    public void setLoggedInPlayer(Player loggedInPlayer) {
+        this.loggedInPlayer = loggedInPlayer;
+    }
+
+    public Enum findCommand(String command) {
+
+        if (command.startsWith("menu enter")) return enterAMenu(command);
+        else if (command.equals("menu exit")) return ShopMenuMessages.EXIT_SHOP_MENU;
+        else if (command.equals("menu show-current")) return ShopMenuMessages.SHOW_MENU;
+        else if (command.startsWith("shop buy")) return buyACard(command);
+        else if (command.equals("shop show --all")) return ShopMenuMessages.SHOW_ALL_CARDS;
+
+        return LoginMenuMessages.INVALID_COMMAND;
+    }
+
+    private Enum enterAMenu(String command) {
+        Matcher matcher = Utils.getMatcher("^menu enter (?i)(Login|MAin|Duel|Deck|Scoreboard|Profile|Shop) Menu$", command);
+        if (!matcher.find()) return ShopMenuMessages.INVALID_COMMAND;
+
+        return ShopMenuMessages.INVALID_NAVIGATION;
+    }
+
+    private Enum buyACard(String command) {
+        Matcher matcher = Utils.getMatcher("^shop buy ([A-Za-z ]+)$", command);
+        if (!matcher.find()) return ShopMenuMessages.INVALID_COMMAND;
+
+        String cardName = matcher.group(1);
+        Card boughtCard = Card.getCardByName(cardName);
+        if (boughtCard == null) return ShopMenuMessages.UNAVAILABLE_CARD;
+        if (boughtCard.getPrice > loggedInPlayer.getMoney()) return ShopMenuMessages.NOT_ENOUGH_MONEY;
+
+        loggedInPlayer.addCardToBoughtCards(boughtCard);
+        return ShopMenuMessages.EMPTY;
+    }
+}
