56329251ed25255aa857c6ead722607a68374045
Amir Mohammad Fakhimi <79265203+AmirMohammadFakhimi@users.noreply.github.com>
2021-04-30 01:48:13 +0430

    phase 0 code

---
+++ phase 0 code/src/main/java/view/LoginMenuView.java
@@ -0,0 +1,19 @@
+package view;
+
+import controller.LoginMenuController;
+import controller.LoginMenuMessages;
+import controller.Utils;
+
+public class LoginMenuView {
+    public static void loginMenuView() {
+        while (true) {
+            String command = Utils.getScanner().nextLine().trim();
+            LoginMenuController loginMenuController = new LoginMenuController();
+            Enum result = loginMenuController.findCommand(command);
+
+            System.out.println(result);
+
+            if (result.equals(LoginMenuMessages.USER_LOGGED_IN)) LoginMenuController.loginUser(command);
+        }
+    }
+}
